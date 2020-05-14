ebf541b85c5ff80ae23c90af9d1e284d69f738b5
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-14 15:47:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(sentence)
   return empty_array.join(" ")
 end
 
+def reverse_each_word(sentence)
+  empty_array = []
+  array = sentence.split(" ")
+  array.collect do |word|
+    empty_array << word.reverse
+  end
+  return empty_array.join(" ")
+end
+
+I'm always close and overthinking
