72d618372f8e9afd54bca655bf9efc50441cc1fd
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-14 15:40:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,18 +5,3 @@ def reverse_each_word(sentence)
     empty_array(word).reverse
   end
 end
-
-# def reverse_word(array)
-
-# Takes in a sentence string
-# Array each word
-# reverse each word loop
-# join sentence
-
-# def reverse_each_word(sentence)
-#   empty_array = []
-#   array = sentence.split(" ")
-#   array.each do |word|
-#     #add each word to the empty_array as you .reverse them
-#   end
-# end
