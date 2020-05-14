e677b4b77fe20f84914e23bff8d393683515b65b
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-14 15:50:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  empty_array = []
-  array = sentence.split(" ")
-  array.each do |word|
-    empty_array << word.reverse
-  end
-  return empty_array.join(" ")
-end
+# def reverse_each_word(sentence)
+#   empty_array = []
+#   array = sentence.split(" ")
+#   array.each do |word|
+#     empty_array << word.reverse
+#   end
+#   return empty_array.join(" ")
+# end
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
